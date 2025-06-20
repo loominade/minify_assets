fce33d904a36ea180b3005a4bea282d3637df3e2
loominade <a.mueller@loom.de>
2025-06-20 11:41:25 +0200

    Update async-css.js
    respect media attribute

--- src/async-css.js
+++ src/async-css.js
@@ -3,6 +3,9 @@
   newStyle.href = style.href
   newStyle.rel = 'preload'
   newStyle.as = 'style'
+  if (style.media) {
+    newStyle.media = style.media
+  }
   style.replaceWith(newStyle)
   newStyle.onload = () => {
     newStyle.rel = 'stylesheet'
